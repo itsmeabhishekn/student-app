329644b89c4696f0b949a3e2a02fbdd001fd2d63
itsmeabhishekn <abhishekn4454@gmail.com>
2022-11-22 01:15:22 +0530

    added delete functionality

--- src/student.java
+++ src/student.java
@@ -24,7 +24,6 @@ class student{
 
     }
 
-
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -32,13 +31,13 @@ class student{
         ArrayList<student> stud_list = new ArrayList<student>();
 
         while(true){
-            System.out.println("Student");
+            System.out.println("student");
             System.out.println("-------------");
             System.out.println();
-            System.out.println("1:Add Student");
-            System.out.println("2:View Student");
-            System.out.println("3:Search Student");
-            System.out.println("4:Delete Student");
+            System.out.println("1:Add student");
+            System.out.println("2:View student");
+            System.out.println("3:Search student");
+            System.out.println("4:Delete student");
             System.out.println("5:Exit");
             System.out.println();
             System.out.println("-------------");
@@ -46,9 +45,8 @@ class student{
 
             int choice=sc.nextInt();
 
-
             switch(choice){
-                case 1:
+                case 1:{
 
                     Scanner sc1 =new Scanner(System.in);
 
@@ -67,6 +65,7 @@ class student{
                     stud_list.add(new student(name, adm_no, roll_no, college));
                     break;
 
+                }
 
                 case 2:{
 
@@ -75,10 +74,7 @@ class student{
                     }
                     break;
 
-
-
-            }
-
+                }
                 case 3:{
                     System.out.println("Enter Admission No: ");
                     int ad_no=sc.nextInt();
@@ -90,6 +86,30 @@ class student{
                         }
                     }
                     break;
+
+                }
+                case 4:{
+                    System.out.println("Enter Admission No: ");
+                    int ad_no=sc.nextInt();
+
+                    for(student i:stud_list){
+
+                        if(i.adm_no == ad_no){
+                            stud_list.remove(i);
+                            break;
+                        }
+                    }
+                    break;
+                }
+
+                case 5:{
+
+                    System.exit(0);
+
+                }
+
+            }
         }
+
     }
 }
